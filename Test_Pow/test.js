a5bd7f3ad5a2db2189f5e3bce1a0e2144b88bf59
Elburevestnik <lisapetrel@mail.ru>
2018-10-12 00:51:25 +0300

    Update test.js
    Corrected syntax errors

--- Test_Pow/test.js
+++ Test_Pow/test.js
@@ -1,9 +1,11 @@
-discribe('doPow', function() {
-	discribe('Возодит x в степень n', function() { 
+describe('pow', function() {
+	
+	describe('Возодит x в степень n', function() { 
+	
 		function makeTest(x) {
 			var expected = x * x * x * x;
-			it('Функия doPow возводит ' + x + ' в степень 4 и получает результат ' + expected, function() {
-				asser.equal(doPow(x, 4), expected);
+			it('Функия pow возводит ' + x + ' в степень 4 и получает результат ' + expected, function() {
+				assert.equal(pow(x, 4), expected);
 			});
 		}
 		for (var x = 1; x <= 6; x++) {
@@ -13,12 +15,12 @@ discribe('doPow', function() {
 
 	});
 	it('Возводя число в отрицательную степень, получим в результате NaN', function() { 
-		asser(isNaN(doPow(2, -3)), 'doPow(2, -3) не NaN');
+		assert(isNaN(pow(2, -3)), 'doPow(2, -3) не NaN');
 	});
 	it('Возводя число в дробную степень, получим в результате NaN', function() { 
-		asser(isNaN(doPow(2, 3.5)), 'doPow(2, -3) не NaN');
+		assert(isNaN(pow(2, 3.5)), 'doPow(2, -3) не NaN');
 	});
-	it("при возведении нуля в нулевую степень результат NaN", function() {
+	it('при возведении нуля в нулевую степень результат NaN', function() {
 		assert(isNaN(pow(0,0)), "") ;
 	});
 });
